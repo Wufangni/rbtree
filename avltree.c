5b9cac634466286957320fe343542b92c8034ec3
Wufangni <niniaab@gmail.com>
2024-06-27 00:21:46 +0800

    Add performance comparison between AVL Tree and Red-Black Tree

--- avltree.c
+++ avltree.c
@@ -1,10 +1,3 @@
-/**
- * AVL树(C语言): C语言实现的AVL树。
- *
- * @author skywang
- * @date 2013/11/07
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "avltree.h"
@@ -12,17 +5,13 @@
 #define HEIGHT(p)    ( (p==NULL) ? -1 : (((Node *)(p))->height) )
 #define MAX(a, b)    ( (a) > (b) ? (a) : (b) )
 
-/*
- * 获取AVL树的高度
- */
+
 int avltree_height(AVLTree tree)
 {
     return HEIGHT(tree);
 }
 
-/*
- * 前序遍历"AVL树"
- */
+
 void preorder_avltree(AVLTree tree)
 {
     if(tree != NULL)
@@ -34,9 +23,7 @@ void preorder_avltree(AVLTree tree)
 }
 
 
-/*
- * 中序遍历"AVL树"
- */
+
 void inorder_avltree(AVLTree tree)
 {
     if(tree != NULL)
@@ -47,9 +34,7 @@ void inorder_avltree(AVLTree tree)
     }
 }
 
-/*
- * 后序遍历"AVL树"
- */
+
 void postorder_avltree(AVLTree tree)
 {
     if(tree != NULL)
@@ -60,9 +45,7 @@ void postorder_avltree(AVLTree tree)
     }
 }
 
-/*
- * (递归实现)查找"AVL树x"中键值为key的节点
- */
+
 Node* avltree_search(AVLTree x, Type key)
 {
     if (x==NULL || x->key==key)
@@ -74,9 +57,7 @@ Node* avltree_search(AVLTree x, Type key)
         return avltree_search(x->right, key);
 }
 
-/*
- * (非递归实现)查找"AVL树x"中键值为key的节点
- */
+
 Node* iterative_avltree_search(AVLTree x, Type key)
 {
     while ((x!=NULL) && (x->key!=key))
@@ -90,9 +71,7 @@ Node* iterative_avltree_search(AVLTree x, Type key)
     return x;
 }
 
-/*
- * 查找最小结点：返回tree为根结点的AVL树的最小结点。
- */
+
 Node* avltree_minimum(AVLTree tree)
 {
     if (tree == NULL)
@@ -103,9 +82,7 @@ Node* avltree_minimum(AVLTree tree)
     return tree;
 }
 
-/*
- * 查找最大结点：返回tree为根结点的AVL树的最大结点。
- */
+
 Node* avltree_maximum(AVLTree tree)
 {
     if (tree == NULL)
@@ -116,11 +93,7 @@ Node* avltree_maximum(AVLTree tree)
     return tree;
 }
 
-/*
- * LL：左左对应的情况(左单旋转)。
- *
- * 返回值：旋转后的根节点
- */
+
 static Node* left_left_rotation(AVLTree k2)
 {
     AVLTree k1;
@@ -135,11 +108,7 @@ static Node* left_left_rotation(AVLTree k2)
     return k1;
 }
 
-/*
- * RR：右右对应的情况(右单旋转)。
- *
- * 返回值：旋转后的根节点
- */
+
 static Node* right_right_rotation(AVLTree k1)
 {
     AVLTree k2;
@@ -154,11 +123,7 @@ static Node* right_right_rotation(AVLTree k1)
     return k2;
 }
 
-/*
- * LR：左右对应的情况(左双旋转)。
- *
- * 返回值：旋转后的根节点
- */
+
 static Node* left_right_rotation(AVLTree k3)
 {
     k3->left = right_right_rotation(k3->left);
@@ -166,11 +131,7 @@ static Node* left_right_rotation(AVLTree k3)
     return left_left_rotation(k3);
 }
 
-/*
- * RL：右左对应的情况(右双旋转)。
- *
- * 返回值：旋转后的根节点
- */
+
 static Node* right_left_rotation(AVLTree k1)
 {
     k1->right = left_left_rotation(k1->right);
@@ -178,14 +139,7 @@ static Node* right_left_rotation(AVLTree k1)
     return right_right_rotation(k1);
 }
 
-/*
- * 创建AVL树结点。
- *
- * 参数说明：
- *     key 是键值。
- *     left 是左孩子。
- *     right 是右孩子。
- */
+
 static Node* avltree_create_node(Type key, Node *left, Node* right)
 {
     Node* p;
@@ -200,20 +154,12 @@ static Node* avltree_create_node(Type key, Node *left, Node* right)
     return p;
 }
 
-/*
- * 将结点插入到AVL树中，并返回根节点
- *
- * 参数说明：
- *     tree AVL树的根结点
- *     key 插入的结点的键值
- * 返回值：
- *     根节点
- */
+
 Node* avltree_insert(AVLTree tree, Type key)
 {
     if (tree == NULL)
     {
-        // 新建节点
+
         tree = avltree_create_node(key, NULL, NULL);
         if (tree==NULL)
         {
@@ -221,10 +167,9 @@ Node* avltree_insert(AVLTree tree, Type key)
             return NULL;
         }
     }
-    else if (key < tree->key) // 应该将key插入到"tree的左子树"的情况
+    else if (key < tree->key)
     {
         tree->left = avltree_insert(tree->left, key);
-        // 插入节点后，若AVL树失去平衡，则进行相应的调节。
         if (HEIGHT(tree->left) - HEIGHT(tree->right) == 2)
         {
             if (key < tree->left->key)
@@ -233,10 +178,9 @@ Node* avltree_insert(AVLTree tree, Type key)
                 tree = left_right_rotation(tree);
         }
     }
-    else if (key > tree->key) // 应该将key插入到"tree的右子树"的情况
+    else if (key > tree->key)
     {
         tree->right = avltree_insert(tree->right, key);
-        // 插入节点后，若AVL树失去平衡，则进行相应的调节。
         if (HEIGHT(tree->right) - HEIGHT(tree->left) == 2)
         {
             if (key > tree->right->key)
@@ -245,9 +189,9 @@ Node* avltree_insert(AVLTree tree, Type key)
                 tree = right_left_rotation(tree);
         }
     }
-    else //key == tree->key)
+    else
     {
-        printf("添加失败：不允许添加相同的节点！\n");
+        printf("Adding the same node is not allowed！\n");
     }
 
     tree->height = MAX( HEIGHT(tree->left), HEIGHT(tree->right)) + 1;
@@ -255,25 +199,17 @@ Node* avltree_insert(AVLTree tree, Type key)
     return tree;
 }
 
-/*
- * 删除结点(z)，返回根节点
- *
- * 参数说明：
- *     ptree AVL树的根结点
- *     z 待删除的结点
- * 返回值：
- *     根节点
- */
+
 static Node* delete_node(AVLTree tree, Node *z)
 {
-    // 根为空 或者 没有要删除的节点，直接返回NULL。
+
     if (tree==NULL || z==NULL)
         return NULL;
 
-    if (z->key < tree->key)        // 待删除的节点在"tree的左子树"中
+    if (z->key < tree->key)
     {
         tree->left = delete_node(tree->left, z);
-        // 删除节点后，若AVL树失去平衡，则进行相应的调节。
+
         if (HEIGHT(tree->right) - HEIGHT(tree->left) == 2)
         {
             Node *r =  tree->right;
@@ -283,10 +219,10 @@ static Node* delete_node(AVLTree tree, Node *z)
                 tree = right_right_rotation(tree);
         }
     }
-    else if (z->key > tree->key)// 待删除的节点在"tree的右子树"中
+    else if (z->key > tree->key)
     {
         tree->right = delete_node(tree->right, z);
-        // 删除节点后，若AVL树失去平衡，则进行相应的调节。
+
         if (HEIGHT(tree->left) - HEIGHT(tree->right) == 2)
         {
             Node *l =  tree->left;
@@ -296,31 +232,18 @@ static Node* delete_node(AVLTree tree, Node *z)
                 tree = left_left_rotation(tree);
         }
     }
-    else    // tree是对应要删除的节点。
+    else 
     {
-        // tree的左右孩子都非空
         if ((tree->left) && (tree->right))
         {
             if (HEIGHT(tree->left) > HEIGHT(tree->right))
             {
-                // 如果tree的左子树比右子树高；
-                // 则(01)找出tree的左子树中的最大节点
-                //   (02)将该最大节点的值赋值给tree。
-                //   (03)删除该最大节点。
-                // 这类似于用"tree的左子树中最大节点"做"tree"的替身；
-                // 采用这种方式的好处是：删除"tree的左子树中最大节点"之后，AVL树仍然是平衡的。
                 Node *max = avltree_maximum(tree->left);
                 tree->key = max->key;
                 tree->left = delete_node(tree->left, max);
             }
             else
             {
-                // 如果tree的左子树不比右子树高(即它们相等，或右子树比左子树高1)
-                // 则(01)找出tree的右子树中的最小节点
-                //   (02)将该最小节点的值赋值给tree。
-                //   (03)删除该最小节点。
-                // 这类似于用"tree的右子树中最小节点"做"tree"的替身；
-                // 采用这种方式的好处是：删除"tree的右子树中最小节点"之后，AVL树仍然是平衡的。
                 Node *min = avltree_maximum(tree->right);
                 tree->key = min->key;
                 tree->right = delete_node(tree->right, min);
@@ -337,15 +260,6 @@ static Node* delete_node(AVLTree tree, Node *z)
     return tree;
 }
 
-/*
- * 删除结点(key是节点值)，返回根节点
- *
- * 参数说明：
- *     tree AVL树的根结点
- *     key 待删除的结点的键值
- * 返回值：
- *     根节点
- */
 Node* avltree_delete(AVLTree tree, Type key)
 {
     Node *z;
@@ -355,9 +269,6 @@ Node* avltree_delete(AVLTree tree, Type key)
     return tree;
 }
 
-/*
- * 销毁AVL树
- */
 void destroy_avltree(AVLTree tree)
 {
     if (tree==NULL)
@@ -371,20 +282,11 @@ void destroy_avltree(AVLTree tree)
     free(tree);
 }
 
-/*
- * 打印"AVL树"
- *
- * tree       -- AVL树的节点
- * key        -- 节点的键值
- * direction  --  0，表示该节点是根节点;
- *               -1，表示该节点是它的父结点的左孩子;
- *                1，表示该节点是它的父结点的右孩子。
- */
 void print_avltree(AVLTree tree, Type key, int direction) {
     if(tree != NULL) {
-        if(direction == 0)    // tree是根节点
+        if(direction == 0) 
             printf("%2d is root\n", tree->key);
-        else                // tree是分支节点
+        else 
             printf("%2d is %2d's %6s child\n", tree->key, key, direction==1?"right" : "left");
 
         print_avltree(tree->left, tree->key, -1);
